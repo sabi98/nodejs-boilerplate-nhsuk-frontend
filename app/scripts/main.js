dccb464895628b209333804bc4512889ec77b8df
Sabeeh UL-Hassan <sabeeh.ul-hassan1@nhs.net>
2020-04-05 19:25:24 +0100

    Update packages

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,10 +2,8 @@
 // NHS.UK frontend
 import nhsuk_header from '../../node_modules/nhsuk-frontend/packages/components/header/header';
 import nhsuk_skipLink from '../../node_modules/nhsuk-frontend/packages/components/skip-link/skip-link';
-import autocomplete from '../../node_modules/nhsuk-frontend/packages/components/header/autocomplete';
 
 // HTML5 polyfills
-import '../../node_modules/nhsuk-frontend/packages/components/details/details.polyfill';
 
 // Initialise components
 nhsuk_header();
